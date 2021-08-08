96a4ee6b6174ec56aefbfce8fb68800d0e9a10a6
Dialvec <diavelandiaca@unal.edu.co>
2021-08-07 21:01:17 -0500

    Update main.py
    First version constructor

--- main.py
+++ main.py
@@ -5,72 +5,92 @@ Created on Sat Aug  7 00:01:51 2021
 
 @author: Dialvec
 """
-#Packages importing
-missing_packages=[]
 
 import sys
 
-try:
-    import os
-except ModuleNotFoundError:
-    sys.exit("""Library os not found 
-                 Please install missing library""")
-
-#Working directory setup for local modules
-cwd = os.getcwd()
-print("Current Working Directory ->" , cwd)
-os.chdir(cwd)
+def Lnk_scrapper(username, password, companies):
     
-try:
-    import utils
-except ModuleNotFoundError:
-    sys.exit("""Custom library utils.py not found 
-                 Please check project files""")
-
-try:
-    import pandas as pd
-except ModuleNotFoundError:
-    missing_packages.append("Pandas")
+    #missing packages importing
+    missing_packages=[]
     
-try:
-    from datetime import date
-except ModuleNotFoundError:
-    missing_packages.append("datetime")
+    try:
+        import os
+    except ModuleNotFoundError:
+        sys.exit("""Library os not found 
+                     Please install missing library""")
     
-try:
-    import selenium
-except ModuleNotFoundError:
-    missing_packages.append("selenium")
-
-try:
-    import LnkDriver
-except ModuleNotFoundError:
-    missing_packages.append("LnkDriver.py")
+    #Working directory setup for local modules
+    cwd = os.getcwd()
+    print("Current Working Directory ->" , cwd)
+    os.chdir(cwd)
+        
+    try:
+        import utils
+    except ModuleNotFoundError:
+        sys.exit("""Custom library utils.py not found 
+                     Please check project files""")
     
-try:
-    import LnkCrawler
-except ModuleNotFoundError:
-    missing_packages.append("LnkCrawler.py")
+    try:
+        import pandas as pd
+    except ModuleNotFoundError:
+        missing_packages.append("Pandas")
+        
+    try:
+        from datetime import date
+    except ModuleNotFoundError:
+        missing_packages.append("datetime")
+        
+    try:
+        import selenium
+    except ModuleNotFoundError:
+        missing_packages.append("selenium")
+    
+    try:
+        import LnkDriver
+    except ModuleNotFoundError:
+        missing_packages.append("LnkDriver.py")
+        
+    try:
+        import LnkCrawler
+    except ModuleNotFoundError:
+        missing_packages.append("LnkCrawler.py")
+    
+    try:
+        import Database
+    except ModuleNotFoundError:
+        missing_packages.append("Database.py")
+        
+    def cleanup_companies(companies):
+        for i in range(len(companies)):
+            companies[i] = companies[i].lower().replace(" ", "")
+        return companies
+    
+    companies = cleanup_companies(companies)
+    
+    utils.moduleNotFoundExit(missing_packages)
 
-try:
-    import Database
-except ModuleNotFoundError:
-    missing_packages.append("Database.py")
-
-utils.moduleNotFoundExit(missing_packages)
-
-Database = Database.NewDatabase(df_path = utils.FILE_PATH)
-
-Driver = LnkDriver.NewLnkDriver('daggercrow@gmail.com', '57FtjCIbbZ')
-Driver.startSession()
-
-MyCrawler = LnkCrawler.NewLnkCrawler(Driver)
-df_chunk = MyCrawler.scanCompaniesSalesOpenings(['google', 'microsoft', 'amazon'])
-
-Database.add_chunk(df_chunk)
-Database.save()
-
-Driver.endSession()
-
-print("Excecution finished")
+    Database = Database.NewDatabase(df_path = utils.FILE_PATH)
+    
+    Driver = LnkDriver.NewLnkDriver(username, password)
+    Driver.startSession()
+    
+    MyCrawler = LnkCrawler.NewLnkCrawler(Driver)
+    df_chunk = MyCrawler.scanCompaniesSalesOpenings(companies)
+    
+    Database.add_chunk(df_chunk)
+    Database.save()
+    
+    Driver.endSession()
+    
+    print("Excecution finished")
+    
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    companies=[]
+    
+    for i in range(3, len(sys.argv)):
+        companies.append(sys.argv[i])
+        
+    Lnk_scrapper(username, password, companies)
 
